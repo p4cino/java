0f4b0c7645f1c202082b7e2e69b53c901b91ab73
P4cino <piro.dbv.pl@gmail.com>
2016-12-06 21:18:43 +0100

    Test11

--- src/test/java/pl/lodz/uni/math/AccountTest.java
+++ src/test/java/pl/lodz/uni/math/AccountTest.java
@@ -7,6 +7,7 @@ package pl.lodz.uni.math;
 
 import java.util.Date;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -15,13 +16,45 @@ import static org.junit.Assert.*;
  * @author wojte
  */
 public class AccountTest {
-
+    
     public AccountTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        Client ClientAndrzej = new Client("Andrzej", "Zaucha", "Lodz");
+        Client ClientJanusz = new Client("Janusz", "Tracz", "Lodz");
     }
 
     @Test
-    public void doCheck() {
-        AccountTest ac1 = new AccountTest();
+    public void testDoTransaction_8args() {
+        System.out.println("doTransaction");
+        String countr = "";
+        String swift = "";
+        Account account = null;
+        String description = "";
+        Date date = null;
+        Double amount = null;
+        int transactionNumber = 0;
+        TransactionTypeDictionary transactionType = null;
+        Account instance = null;
+        instance.doTransaction(countr, swift, account, description, date, amount, transactionNumber, transactionType);
+        fail("The test case is a prototype.");
     }
 
+    @Test
+    public void testDoTransaction_6args() {
+        System.out.println("doTransaction");
+        Account account = null;
+        String description = "";
+        Date date = null;
+        Double amount = null;
+        int transactionNumber = 0;
+        TransactionTypeDictionary transactionType = null;
+        Account instance = null;
+        instance.doTransaction(account, description, date, amount, transactionNumber, transactionType);
+        fail("The test case is a prototype.");
+    }
+
+    
 }
